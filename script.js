9d4ccbf0033b47f7b48be1455780e07e5fbe73af
josephrfarmer <josephr.farmer24@gmail.com>
2018-08-10 16:08:02 -0400

    step one is complete of project
    forgot to commit since first commit
    working on step two now
    functions for step 2 are finished.

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
+// document.getElementById('parking-form').addEventListener('submit', function (event){
+//     event.preventDefault()
+
+//     var inputFields = document.getElementsByTagName('input')
+    
+
+//     for(var i = 0; i < inputFields.length; i++ ){
+
+//         inputFields[i].parentElement.classList.remove("input-invalid","input-valid" )
+
+//     if (inputFields[i].value.trim() === ''){
+//         inputFields[i].parentElement.classList.add("input-invalid")
+//     } else {
+//         inputFields[i].parentElement.classList.add("input-valid")
+//     }
+// }
+// })
+
 document.getElementById('parking-form').addEventListener('submit', function (event){
     event.preventDefault()
-};
 
-var nameField = document.getElementById('name-field').value.trim()
+    var inputFields = document.getElementsByTagName('input')
+    
+
+    for(var i = 0; i < inputFields.length; i++ ){
+        inputFields[i].parentElement.classList.remove("input-invalid","input-valid" )
+    }
+
+    for(var i = 0; i < inputFields.length; i++ ){
+
+    if (inputFields[i].value.trim() === ''){
+        if (inputFields[i].parentElement.classList.contains("input-valid")){
+            inputFields[i].parentElement.classList.add("input-invalid")
+            inputFields[i].parentElement.classList.remove("input-valid")
+        } else {
+            inputFields[i].parentElement.classList.add("input-invalid")
+        }
+    } else {
+        if(!inputFields[i].parentElement.classList.contains("input-invalid")){
+            inputFields[i].parentElement.classList.add("input-valid")
+        }
+    }
+}
+
+function showemptymessage(){
+    var errorMessageDiv = document.createElement('div')
+    errorMessageDiv.classList.add('errorMessage')
+    errorMessageDiv.innerText('is required,')
+}
+
+function clearEmptyMessage(){
+    var element = document.getElementsByClassName('errorMessage')
+    element.parentElement.removeChild(element)
+}
 
 
+})
