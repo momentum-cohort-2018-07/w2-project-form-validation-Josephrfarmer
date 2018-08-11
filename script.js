a0bbd534dc7ef2351d7a1f6e0071be11a982f40d
josephrfarmer <josephr.farmer24@gmail.com>
2018-08-11 13:53:09 -0400

    if field is invalid a message now shows
    need to still fix issue with car field to put message below
    after that I can move on to include the field name before the
    message

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// document.getElementById('parking-form').addEventListener('submit', function (event){
-//     event.preventDefault()
-
-//     var inputFields = document.getElementsByTagName('input')
-
-
-//     for(var i = 0; i < inputFields.length; i++ ){
-
-//         inputFields[i].parentElement.classList.remove("input-invalid","input-valid" )
-
-//     if (inputFields[i].value.trim() === ''){
-//         inputFields[i].parentElement.classList.add("input-invalid")
-//     } else {
-//         inputFields[i].parentElement.classList.add("input-valid")
-//     }
-// }
-// })
-
 document.getElementById('parking-form').addEventListener('submit', function (event) {
     event.preventDefault()
 
@@ -26,16 +8,25 @@ document.getElementById('parking-form').addEventListener('submit', function (eve
         inputFields[i].parentElement.classList.remove("input-invalid", "input-valid")
     }
 
+    for(element of document.querySelectorAll('.errorMessage')){
+        element.remove()
+    }
+
+
     for (var i = 0; i < inputFields.length; i++) {
+
+        // debugger
 
         if (inputFields[i].value.trim() === '') {
             if (inputFields[i].parentElement.classList.contains("input-valid")) {
                 inputFields[i].parentElement.classList.add("input-invalid")
                 inputFields[i].parentElement.classList.remove("input-valid")
+                showemptymessage(inputFields[i])
             } else {
                 inputFields[i].parentElement.classList.add("input-invalid")
+                showemptymessage(inputFields[i])
             }
-            
+
         } else {
             if (!inputFields[i].parentElement.classList.contains("input-invalid")) {
                 inputFields[i].parentElement.classList.add("input-valid")
@@ -43,21 +34,13 @@ document.getElementById('parking-form').addEventListener('submit', function (eve
         }
     }
 
-    function showemptymessage() {
+    function showemptymessage(input) {
         var errorMessageDiv = document.createElement('div')
         errorMessageDiv.classList.add('errorMessage')
-        errorMessageDiv.innerText('is required,')
+        errorMessageDiv.innerText = 'is required'
+        input.parentElement.appendChild(errorMessageDiv)
     }
 
-    function clearEmptyMessage() {
-        var element = document.getElementsByClassName('errorMessage')
-        element.parentElement.removeChild(element)
-    }
-
-    var inputText = []
-
-    for (var i = 0; i < inputFields.length; i++){
-        inputText = document.getElementsByTagName('input').innerText
-    }
+    
 
 })
